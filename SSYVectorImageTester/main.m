d0298b76191a9f6a2dcddfe286e726123275099f
Jerry Krinock <jerry@sheepsystems.com>
2015-11-30 07:35:07 -0800

    Initial Commit

---
+++ SSYVectorImageTester/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SSYVectorImageTester
+//
+//  Created by Jerry on 15/11/30.
+//  Copyright © 2015 Jerry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
